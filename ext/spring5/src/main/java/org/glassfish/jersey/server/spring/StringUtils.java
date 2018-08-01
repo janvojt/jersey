3f58b3a13944666ef8d8b0b4b394650e0d0ccfd8
Jan Vojt <jan@vojt.net>
2018-08-01 21:44:29 +0200

    Fix checkstyle issue with trailing whitespace.

--- ext/spring5/src/main/java/org/glassfish/jersey/server/spring/StringUtils.java
+++ ext/spring5/src/main/java/org/glassfish/jersey/server/spring/StringUtils.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 /**
  * Since StringUtils of spring framework have been removed, this is a copy
  * of implementation from last stable 4.x version.
- * 
+ *
  * @author Askar Akhmerov
  */
 public class StringUtils {
